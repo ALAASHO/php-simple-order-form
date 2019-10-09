5cf9a1e87336a658bded35abded6356fff48ca66
ALAA <alaashokri93@gmail.com>
2019-10-09 16:49:01 +0200

    up2

--- form-view.php
+++ form-view.php
@@ -1,8 +1,8 @@
 <?php
-$nameErr = $emailErr  = "";
-$name = $email  = "";
-$submsg =  "";
-$city  = $street = $streetnum = "";
+$nameErr = $emailErr = "";
+$name = $email = "";
+$submsg = "";
+$city = $street = $streetnum = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -16,108 +16,124 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //        }
 //    }
 
-    if(empty($_POST["city"])){
-        echo $cityErr = '<div class="alert alert-danger" role="alert">
-                   City is required
-                        </div>';
-    } else{
+    if (empty($_POST["city"])) {
+        echo $cityErr = '<div class="alert alert-danger" role="alert">City is required</div>';
+    } else {
         $city = $_POST["city"];
     }
 
-    if(empty($_POST["street"])){
+    if (empty($_POST["street"])) {
 
-        echo $strErr = '<div class="alert alert-danger" role="alert">
-                   Street is required
-                        </div>';
-    } else{
+        echo $strErr = '<div class="alert alert-danger" role="alert">Street is required</div>';
+    } else {
         $street = $_POST["street"];
     }
 
-    if(empty($_POST["streetnumber"])){
-        echo $strnumErr = '<div class="alert alert-danger" role="alert">
-                   Street number is required
-                        </div>';
+    if (empty($_POST["streetnumber"])) {
+        echo $strnumErr = '<div class="alert alert-danger" role="alert">Street number is required</div>';
 
-    } else{
+    } else {
         $streetnum = $_POST["streetnumber"];
         if (is_numeric($streetnum)) {
             $streetnum = $_POST["streetnumber"];
         } else {
-            echo $strnumErr = '<div class="alert alert-danger" role="alert">Street No: Numbers Only accepted</div>';PHP_EOL;
-
-
-
+            echo $strnumErr = '<div class="alert alert-danger" role="alert">Street No: Numbers Only accepted</div>';
+            PHP_EOL;
         }
-
     }
 
 
-
-    if(empty($_POST["zipcode"])){
-       echo $zipcodeErr = '<div class="alert alert-danger" role="alert">Zipcode  is required</div>';    }
-    else {
+    if (empty($_POST["zipcode"])) {
+        echo $zipcodeErr = '<div class="alert alert-danger" role="alert">Zipcode  is required</div>';
+    } else {
         $zipcode = $_POST["zipcode"];
         if (is_numeric($zipcode)) {
             $zipcode = $_POST["zipcode"];
         } else {
-            echo $zipcode = '<div class="alert alert-danger" role="alert">Zipcode : Numbers Only accepted</div>';PHP_EOL;
-
-
-
+            echo $zipcode = '<div class="alert alert-danger" role="alert">Zipcode : Numbers Only accepted</div>';
+            PHP_EOL;
         }
-
     }
 
 
     if (empty($_POST["email"])) {
-        echo $emailErr = '<div class="alert alert-danger" role="alert">Email is required</div>';    } else {
+        echo $emailErr = '<div class="alert alert-danger" role="alert">Email is required</div>';
+    } else {
         $email = test_input($_POST["email"]);
         // check if e-mail address is well-formed
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo $emailErr = '<div class="alert alert-danger" role="alert">Invalid email format</div>';
-        } else{
+        } else {
 //            $emailErr = "good email";
         }
     }
 }
 
 
-function test_input($data) {
+function test_input($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
 
-if(!isset($_POST["submit"])) {
+
+$op1 = date('h:i:s A', time() + 7200);  // 60*60*2
+$op2 = date('h:i:s A', time() + 1440);  // 60*24
+
+$current = date('h:i:s A', time() );//
+//$current = strtotime('now') +;
+//$diffference = ($two_h - $current);
+//echo "<br>  Expected delivery time :  $two_h";
+//echo "<br>  Expected delivery time (Express) :  $two_ex";
+
+
+
+if (!isset($_POST["order"])) {
 } else {
     echo $submsg = '<div class="alert alert-success" role="alert">Thank you for reaching out to us. We will get back to you shortly.</div>';
 
+    if ($_POST["delivery"] == 0 ) {
+        echo '<div class="alert alert-light" role="alert">'.'Expected delivery time '.$op1.' apox. 2 hours</div>';
+    }
+    else {
+        echo '<div class="alert alert-light" role="alert">'.'Expected delivery time '.$op2.' apox. 45 mins</div>';
+    }
+//    echo "<div class="alert alert-warning" role="alert"> <br>  Expected delivery time : {$param} '$two_h' </div>";
+
+
+
 }
 
+/*  cookie
 
 $cookie_str = "street";
 $cookie_str_value = $streetnum;
-setcookie($cookie_str, $cookie_str_value , time() + (86400 * 30), "/");
+setcookie($cookie_str, $cookie_str_value, time() + (86400 * 30), "/");
 
 
-if(!isset($_COOKIE[$cookie_str])) {
+if (!isset($_COOKIE[$cookie_str])) {
     echo "Cookie named '" . $cookie_str . "' is not set!";
 } else {
     echo "Cookie '" . $cookie_str . "' is set!<br>";
     echo "Value is: " . $_COOKIE[$cookie_str];
 }
 
+*/
 
 
-if(empty($_POST['submit']))
-{
-    $_SESSION['email']= $_POST['email'];
-    $_SESSION['street']= $_POST['street'];
-    $_SESSION['city']= $_POST['city'];
-    $_SESSION['zipcode']= $_POST['zipcode'];
-    $_SESSION['streetnumber']= $_POST['streetnumber'];
+if (empty($_POST['submit'])) {
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['street'] = $_POST['street'];
+    $_SESSION['city'] = $_POST['city'];
+    $_SESSION['zipcode'] = $_POST['zipcode'];
+    $_SESSION['streetnumber'] = $_POST['streetnumber'];
 }
+
+
+
+
 ?>
 
 
@@ -148,22 +164,22 @@ if(empty($_POST['submit']))
         </ul>
     </nav>
 
-<!--    <div id="contactform-messages" class="part-five part-four" style="display: flex; align-items: center; justify-content: center; background-size: auto !important;">-->
-<!--        <p> --><?php //echo $submsg ;?><!-- </p>-->
-<!--    </div>-->
+    <!--    <div id="contactform-messages" class="part-five part-four" style="display: flex; align-items: center; justify-content: center; background-size: auto !important;">-->
+    <!--        <p> --><?php //echo $submsg ;?><!-- </p>-->
+    <!--    </div>-->
 
     <form method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control" value="<?php echo $_POST['email']?>"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $_POST['email'] ?>"/>
             </div>
             <div>
 
             </div>
-<!--            <div id="hide" class="alert alert-danger" role="alert">-->
-<!--                <span class="error">* --><?php //echo $emailErr."<br/>".$nameErr;?><!--</span>-->
-<!--            </div>-->
+            <!--            <div id="hide" class="alert alert-danger" role="alert">-->
+            <!--                <span class="error">* --><?php //echo $emailErr."<br/>".$nameErr;?><!--</span>-->
+            <!--            </div>-->
 
         </div>
 
@@ -171,31 +187,33 @@ if(empty($_POST['submit']))
             <legend>Address</legend>
 
 
-
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $_POST['street']?>" >
+                    <input type="text" name="street" id="street" class="form-control"
+                           value="<?php echo $_POST['street'] ?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $_POST['streetnumber']?>" >
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                           value="<?php echo $_POST['streetnumber'] ?>">
                 </div>
-<!--                <div class="alert alert-danger" role="alert">-->
-<!--                    <span class="error">* --><?php //echo $strnumErr;?><!--</span>-->
-<!--                </div>-->
+                <!--                <div class="alert alert-danger" role="alert">-->
+                <!--                    <span class="error">* --><?php //echo $strnumErr;?><!--</span>-->
+                <!--                </div>-->
 
 
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $_POST['city']?>">
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $_POST['city'] ?>">
 
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $_POST['zipcode']?>">
+                    <input type="text" id="zipcode" name="zipcode" class="form-control"
+                           value="<?php echo $_POST['zipcode'] ?>">
                 </div>
 
             </div>
@@ -205,15 +223,27 @@ if(empty($_POST['submit']))
             <legend>Products</legend>
             <?php foreach ($products AS $i => $product): ?>
                 <label>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?>
+                    -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br/>
             <?php endforeach; ?>
         </fieldset>
 
-        <button type="submit" name="order" class="btn btn-primary">Order!</button>
-<!--        <input type="submit" name="submit" value="Submit">-->
-    </form>
 
+        <fieldset>
+            <legend>Delivery Method</legend>
+            <select name="delivery" class="form-control col-md-6">
+                <option value="0" selected>Standard Delivery </option>
+                <option value="1">Express Delivery via express drone services</option>
+            </select>
+        </fieldset>
+
+
+        <button type="submit" name="order" class="btn btn-primary">Order!</button>
+        <!--        <input type="submit" name="submit" value="Submit">-->
+    </form>
+    <div>
+    </div>
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
 </div>
 
